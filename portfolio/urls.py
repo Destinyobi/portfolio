420b3a4f8c495dce68505070501a1c976e6febb9
obidestiny937@gmail.com <obidestiny937@gmail.com>
2024-10-03 10:25:03 +0100

    updated admin

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('Dboy/', admin.site.urls),
     path('', include('resume.urls'))
 ]
